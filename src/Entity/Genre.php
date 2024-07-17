163582cc580a99e458e6dd0f7cba1fbcaf14b729
Leonardo1142 <leonardo.courtois@icloud.com>
2024-07-17 14:55:21 +0200

    feat(genre): add entity genre.

---
+++ src/Entity/Genre.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GenreRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+#[ORM\Entity(repositoryClass: GenreRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_GENRE', fields: ['name'])]
+#[UniqueEntity(fields: ['name'], message: "Le genre est déjà utilisé, veuillez en choisir un autre !")]
+class Genre
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?bool $enable = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function isEnable(): ?bool
+    {
+        return $this->enable;
+    }
+
+    public function setEnable(bool $enable): static
+    {
+        $this->enable = $enable;
+
+        return $this;
+    }
+}
