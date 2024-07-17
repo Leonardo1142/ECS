ee6a32994da0a264080c65474e2c84f5976a2731
Leonardo1142 <leonardo.courtois@icloud.com>
2024-07-17 10:02:08 +0200

    feat(user): add controller

---
+++ src/Controller/Backend/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Backend;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/users', name: 'admin.users')]
+class UserController extends AbstractController
+{
+    public function __construct(
+        private EntityManagerInterface $em,
+    )
+    {
+    }
+
+    #[Route('', name: '.index', methods: ['GET'])]
+    public function index(UserRepository $repo): Response
+    {
+        return $this->render('Backend/User/index.html.twig', [
+            'users' => $repo->findAll(),
+        ]);
+    }
+}
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,6 +5,8 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 /**
  * @extends ServiceEntityRepository<User>
@@ -16,6 +18,19 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $user::class));
+        }
+
+        $user->setPassword($newHashedPassword);
+        $this->getEntityManager()->persist($user);
+        $this->getEntityManager()->flush();
+    }
     //    /**
     //     * @return User[] Returns an array of User objects
     //     */
